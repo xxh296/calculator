303b76861982728ab5176d728d63094adadc8f26
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-08 19:26:28 -0800

    Add essential functions and variables * Wrote functions for `add`, `subtract`, `multiply`, `divide`, and `operate`. * Defined variables for `num1`, `num2`, and enum object `Operator`. (These will be used to update the display later.)

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello");
+const num1 = 0;
+const num2 = 0;
+const Operator = {
+    ADD: " + ",
+    SUBTRACT: " - ",
+    MULTIPLY: " * ",
+    DIVIDE: " / ",
+}
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(operation, num1, num2) {
+    switch (operation) {
+        case add:
+            return add(num1, num2);
+        case subtract:
+            return subtract(num1, num2);
+        case multiply:
+            return multiply(num1, num2);
+        case divide:
+            return divide(num1, num2);
+        default:
+            return "ERROR: Invslid input."
+    }
+}
